16e46fa0b8223cd81463c5ebd5c5164cff519264
synkronus <synkronus.software@gmail.com>
2023-06-13 11:15:41 -0500

    ...

--- app/features/static-revalidation/[id]/page.tsx
+++ app/features/static-revalidation/[id]/page.tsx
@@ -1,28 +1,25 @@
 import { notFound } from "next/navigation";
 import { createSupaServerClientComponent } from "@/src/lib/supabase";
 import { cookies } from 'next/headers';
+import { map } from 'lodash';
 
 // cache this page for 1 minute
 export const revalidate = 60;
 
 export async function generateStaticParams() {
   const { data: posts } = await createSupaServerClientComponent(cookies).from("posts").select("id");
-
-  return posts?.map(({ id }) => ({
-    id,
-  }));
+  return map(posts, (p) => ({ id: `${p.id}` })); 
+  
 }
 
-export default async function Post({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  const { data: post } = await createSupaServerClientComponent(cookies)
-    .from("posts")
-    .select()
-    .match({ id })
-    .single();
+async function fetchPost(id: string) {
+  const {data, error} = await createSupaServerClientComponent(cookies)
+    .from("posts").select().match({ id }).maybeSingle(); 
+  return data as any;
+}
+
+export default async function Post({ params: { id }, }: { params: { id: string }; }) {
+  const post = await fetchPost(id);
 
   if (!post) {
     notFound();
